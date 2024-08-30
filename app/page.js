ba355d4feafbb08642211e2c6b3485d8667d2e0b
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2024-08-31 01:02:19 +0530

    minor changes done

--- app/page.js
+++ app/page.js
@@ -1,18 +1,19 @@
-import { FingerprintIcon, LockIcon } from "lucide-react";
 import Link from "next/link";
-
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex items-center justify-center h-screen w-full mx-auto bg-indigo-900">
-      <div className="max-w-lg p-4 rounded-lg flex flex-col justify-center items-center">
-        {/* <FingerprintIcon className="text-indigo-500 w-24 h-24" /> */}
-        <div className="text-center max-w-screen-sm pt-4 pb-10">
-          {/* <h1 className="text-white font-bold text-2xl">Next Protect</h1> */}
-          
-        </div>
-        <div className="flex space-x-3">
+    <section class="h-screen">
+      <div class="h-full">
+        <div class="flex h-full flex-wrap items-center justify-center lg:justify-between">
+          <div class="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+            <img
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/authentication/illustration.svg"
+              class="w-full"
+              alt="Sample image"
+            />
+          </div>
+
+          <div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
           <Link
             href="/dashboard"
             className="inline-flex items-center rounded-md bg-white px-4 py-2 text-sm font-semibold space-x-1 text-indigo-700 shadow-sm hover:bg-white/90"
@@ -20,9 +21,10 @@ export default function Home() {
             <LockIcon className="w-4 h-4" />
             <span>Go to Sign In Page</span>
           </Link>
+
+          </div>
         </div>
       </div>
-    </div>
-    </div>
+    </section>
   );
 }
